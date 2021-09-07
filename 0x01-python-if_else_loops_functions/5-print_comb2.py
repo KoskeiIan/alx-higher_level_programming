bb911b824f1c1c63204fa8bc1c1603e685b6aa37
koskeiian <ian.k.koskei@gmail.com>
2021-09-08 00:08:10 +0300

    prints 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 98:
-        print(i)
-    else:
-        print("{:0 > 2d}".format(i), end = ", ")
+for number in range(0, 100):
+        if int != 99:
+                print('{:02d},'.format(number), end=" ")
+        else:
+                print('{}'.format(number))
