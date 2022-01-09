d24bf595591a10bf102f1db1c6ef38ad15fcb0f8
KoskeiIan <ian.k.koskei@gmail.com>
2022-01-09 23:37:22 +0300

    github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Github credentials"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/user', auth=(argv[1],
+                            argv[2]))
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        response = response.json()
+        print(response.get('id'))
