86954998ceb679348abbfade5e5f6e4e6eb033aa
KoskeiIan <ian.k.koskei@gmail.com>
2022-01-09 23:57:42 +0300

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Get body size from outgoing request to url introduced as argument
-
 curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# body of an url
+curl -sfL "$1" -X GET
