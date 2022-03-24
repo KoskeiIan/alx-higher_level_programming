850bfe4648328aaab2ea52f3fff74011de640357
KoskeiIan <ian.k.koskei@gmail.com>
2022-03-24 11:55:18 +0300

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Creates a user_od_1 and has all the privileges
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_od_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+-- MYSQL script that creates the MySQL server user user_0d_1: should have all
+-- privileges on MySQL server, password should be set to user_0d_1_pwd.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
