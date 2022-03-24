e761ca27a015a97477950d00beab37e1f35f8644
KoskeiIan <ian.k.koskei@gmail.com>
2022-03-24 13:11:52 +0300

    correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get body size from outgoing request to url introduced as argument
-curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
+curl -sI "$1" | grep 'Content-Length' | cut -c 17-
